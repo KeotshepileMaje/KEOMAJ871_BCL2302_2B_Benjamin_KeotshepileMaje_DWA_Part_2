21a9ecffe163415aa6293eb4c943138ed74e5016
keotshepileMaje <keotshepilemaje@gmail.com>
2023-07-17 20:00:10 +0200

    box challenge

---
+++ DWA_17/Challenges/boxes_challenge/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Box from './box.jsx'
+import boxes from './boxes'
+
+import './App.css'
+
+
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    function toggle(id) {
+        setSquares(prevSquares => {
+            return prevSquares.map(
+              (square) => {
+                return square.id === id ? {
+                  ...square,
+                  on : !square.on
+                } : square
+            })
+        })
+    }
+  
+    const squareElements = squares.map(square => (
+        <Box 
+            key = {square.id} 
+            on = {square.on} 
+            toggle={() => toggle(square.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
